01bff16137d7ffd08a0d95f390ff215cf333cebf
Volodymyr Kharuk <vkh@melexis.com>
2016-10-05 07:33:33 +0300

    Test with windowsize

--- test/graphic/test.py
+++ test/graphic/test.py
@@ -5,11 +5,14 @@ from tkinter import messagebox
 
 top = tkinter.Tk()
 
+top.title("Huge test")
+top.geometry('{}x{}'.format(1000, 300))
+
 def helloCallBack():
    messagebox.showinfo( "Hello Python", "Hello World")
 
 B = tkinter.Button(top, text ="Hello", command = helloCallBack)
-
 B.pack()
+
 top.mainloop()
 
