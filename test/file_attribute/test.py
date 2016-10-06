158f20d4c24afd8b80daf3f909cada1a1c06cb89
Volodymyr Kharuk <vkh@melexis.com>
2016-10-06 08:57:37 +0300

    Tested function for getting time from file attribute

--- test/file_attribute/test.py
+++ test/file_attribute/test.py
@@ -1,52 +1,11 @@
-# added.sort(key=lambda x: os.stat(os.path.join(path_to_watch, x)).st_mtime)
-#
-#
-#
-#
-# import os, time
-# path_to_watch = 'c://Users//seplema//Documents//arvuti'
-# before = dict([(f, None) for f in os.listdir (path_to_watch)])
-# while 1:
-#     after = dict([(f, None) for f in os.listdir (path_to_watch)])
-#     added = [f for f in after if not f in before]
-#     if before == after:
-#         1==1
-#     else:
-#         if len(added)==1:
-#             print added[0]
-#         else:
-#             for i in range (0,len(added)):
-#                 print added[i]
-#     time.sleep(10)
-#     before = after
-#
-#
-#
-#
-#
-#
-#
-#
-#
-#
-# import os
-#
-# search_dir = "/mydir/"
-# os.chdir(search_dir)
-# files = filter(os.path.isfile, os.listdir(search_dir))
-# files = [os.path.join(search_dir, f) for f in files] # add path to each file
-# files.sort(key=lambda x: os.path.getmtime(x))
-#
-#
-#
-#
-# http://effbot.org/zone/python-fileinfo.htm
-#
-#
 
-import os
+import os, datetime
+
+def get_time_from_file(filename):
+    str_time = str(datetime.datetime.fromtimestamp(os.path.getctime(filename)))
+    return str_time.split(' ')[0].split('-')
+
 for filename in os.listdir('.'):
     print (filename)
-    info = os.stat(filename)
-    print(info.st_mtime)
+    print(get_time_from_file(filename))
 
